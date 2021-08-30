94198a5063e279883952a34982c8c0c34ccca7c6
Hari <harikrushnappatel@gmail.com>
2021-08-30 18:31:06 +0530

    new

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { routes } from './routes/routes';
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
 app.use(mw.globalMiddleware); 
-app.use(routes);
+app.use(routes);    
 app.listen(PORT, (): void => {
     console.log(`Server Running here 👉 http://localhost:${PORT}`);
 });
